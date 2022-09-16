85d15e8a5fbbe4453c8ee915bff0b2de86117f85
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-09-16 16:09:54 +0300

    Add project files.

---
+++ CaseSiparis.DataEntity/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseSiparis.DataEntity
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CompanyID { get; set; }
+        public int ProductID { get; set; }
+
+    }
+}
---
+++ CaseSiparis.DataEntity/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseSiparis.DataEntity
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductCount { get; set; }
+        public decimal ProductAmount { get; set; }
+    }
+}
