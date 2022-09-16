2ef58167341cedf9c5ab731009346d2154b1a4e4
Mehmet DEMİR <mmt.demr42@gmail.com>
2022-09-16 16:17:49 +0300

    DataEntityLayer added

--- CaseSiparis.DataEntity/Concrete/Order.cs
+++ CaseSiparis.DataEntity/Concrete/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace CaseSiparis.DataEntity
 {
     public class Order
     {
+        [Key]
         public int OrderID { get; set; }
         public DateTime OrderDate { get; set; }
         public int CompanyID { get; set; }
--- CaseSiparis.DataEntity/Concrete/Product.cs
+++ CaseSiparis.DataEntity/Concrete/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace CaseSiparis.DataEntity
 {
     public class Product
     {
+        [Key]
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int ProductCount { get; set; }
